b397ecda5e33ee210d5b69d342bce3068431f080
Tanaphat Chatwarin <tanaphat.cwr@gmail.com>
2020-10-24 18:44:56 +0700

    add health check endpoint

--- Lab 2/app.js
+++ Lab 2/app.js
@@ -9,6 +9,7 @@ var delay = 10000 + Math.floor(Math.random() * 5000)
 app.get('/', function(req, res) {
   res.send('Hello world from ' + hostname + '! Great job getting the second stage up and running!\n')
 })
+
 app.get('/healthz', function(req, res) {
   if ((Date.now() - startTime) > delay) {
     res.status(500).send({
@@ -18,6 +19,11 @@ app.get('/healthz', function(req, res) {
     res.send('OK!')
   }
 })
+
+app.get('/ping', function(req, res) {
+	res.send('OK!')
+})
+
 app.listen(8080, function() {
   console.log('Sample app is listening on port 8080.')
 })
